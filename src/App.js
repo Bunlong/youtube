2ab5c6d9755f1075b74c34719d75dd1ca8c524eb
Bunlong <bunlong.van@gmail.com>
2017-06-03 22:18:56 +0700

    Remove twitter bootstrap

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail'
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const API_KEY = 'AIzaSyDuY6D-vz3teyoSqZ2c5aHM418fF1_PNC0';
 
